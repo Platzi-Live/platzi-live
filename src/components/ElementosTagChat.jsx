884974991402063b4601963a04b79ece34e2f05c
nancyzj <ancizj@hotmail.com>
2017-08-15 15:24:29 -0500

    tienen las tags

---
+++ src/components/ElementosTagChat.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../App.css'
+
+export default class ElementosTagChat extends Component {
+	render() {
+		return(
+				<div className="col-xs-3 col-md-4 estiloTags">
+					<li className="active color-azul texto-centrado">
+						<a href="#" className="texto-blanco">{this.props.textoTagChat}</a>
+					</li>
+				</div>
+			);
+	}
+}
--- src/containers/ContenedorTabs.jsx
+++ src/containers/ContenedorTabs.jsx
@@ -1,33 +1,53 @@
 import React, {Component} from 'react';
+import ElementosTagChat from "../components/ElementosTagChat";
 import '../ContenedorTabs.css';
 
 export default class ContenedorChat extends Component{
   render() {
+  	const arregloTags = [
+	  	{
+	  		titulo: 'Notas'
+	  	},
+	  	{
+	  		titulo:'Menciones'
+	  	},
+	  	{
+	  		titulo:'Mis Notas'
+	  	}
+  	]
     return(
-		<div className="row">
-			<div className="panel with-nav-tabs panel-primary">
-			    <div className="panel-heading">
-			            <ul className="nav nav-tabs">
-			                <li className="active"><a href="#tab1primary" data-toggle="tab">Notas</a></li>
-			                <li><a href="#tab2primary" data-toggle="tab">Mejores</a></li>
-			                <li><a href="#tab3primary" data-toggle="tab">Preguntas</a></li>
-			                <li><a href="#tab3primary" data-toggle="tab">Enlaces</a></li>
-			            </ul>
-			    </div>
-			    <div className="panel-body">
-			        <div className="tab-content">
-			            <textarea id="txtArea"  placeholder="Escribe una nota">
-			            </textarea>
-			            <div className="pull-right">
-			            	<p id="caracteres">140</p>
-			           </div>
-			        </div>
-			    </div>
-			</div>
+    	<div>
 			<div className="row">
-				
+				<div className="panel with-nav-tabs panel-primary">
+				    <div className="panel-heading">
+				            <ul className="nav nav-tabs">
+				                <li className="active"><a href="#tab1primary" data-toggle="tab">Notas</a></li>
+				                <li><a href="#tab2primary" data-toggle="tab">Mejores</a></li>
+				                <li><a href="#tab3primary" data-toggle="tab">Preguntas</a></li>
+				                <li><a href="#tab3primary" data-toggle="tab">Enlaces</a></li>
+				            </ul>
+				    </div>
+				    <div className="panel-body">
+				        <div className="tab-content">
+				            <textarea id="txtArea"  placeholder="Escribe una nota">
+				            </textarea>
+				            <div className="pull-right">
+				            	<p id="caracteres">140</p>
+				           </div>
+				        </div>
+						<div className="row">
+							<div className="col-sm-12">
+								<ul className="nav nav-tabs">
+									{arregloTags.map((item,indice) =>
+										<ElementosTagChat textoTagChat={item.titulo}
+										key={indice}/>)};
+								</ul>
+							</div>
+						</div>
+				    </div>
+				</div>
 			</div>
-		</div>
+    	</div>
     	);
   }
 }
