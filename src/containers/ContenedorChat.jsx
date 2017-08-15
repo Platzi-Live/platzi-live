00bbcfda47b23e76c05689ae20d7c3dde29d5120
nancyzj <ancizj@hotmail.com>
2017-08-15 17:16:23 -0500

    mantiene la posicion fija

--- src/containers/ContenedorChat.jsx
+++ src/containers/ContenedorChat.jsx
@@ -6,8 +6,8 @@ import ContenedorTabs from './ContenedorTabs.jsx';
 export default class ContenedorChat extends Component {
   render() {
     return(
-			<div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <EstudiantesDestacados />
+			<div className="col-md-offset-8 col-xs-12 col-sm-4 col-md-4 col-lg-4 moviento-fijo">
+        		<EstudiantesDestacados />
 				<ContenedorTabs />
 			</div>
 
