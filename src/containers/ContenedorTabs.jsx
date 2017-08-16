f4ab9ee5810e85a68ca05871a0da574f4919e609
pilarfc <pilar.fc@outlook.com>
2017-08-16 19:34:49 +0000

    no sirve

--- src/containers/ContenedorTabs.jsx
+++ src/containers/ContenedorTabs.jsx
@@ -25,25 +25,25 @@ export default class ContenedorTabs extends Component{
         ]
     }
 
-    // this.actualizarContador = this.actualizarContador.bind(this);
+    this.actualizarContador = this.actualizarContador.bind(this);
     this.agregarComentario = this.agregarComentario.bind(this);
     this.imprimir = this.imprimir.bind(this);
 
 
 }
 
-    // actualizarContador(e){
-    //     const valorInput = e.target.value.length;
-    //
-    //
-    //     let valorEstado = this.state.contador;
-    //     valorEstado = 140 - valorInput;
-    //      this.setState({
-    //         contador: valorEstado
-    //     })
-    //     console.log(valorEstado);
-    //     this.obtenerValorTexto(e);
-    // }
+     actualizarContador(e){
+         const valorInput = e.target.value.length;
+    
+    
+         let valorEstado = this.state.contador;
+         valorEstado = 140 - valorInput;
+          this.setState({
+             contador: valorEstado
+         })
+         console.log(valorEstado);
+         this.obtenerValorTexto(e);
+     }
 
 
      obtenerValorTexto(e){
@@ -52,14 +52,13 @@ export default class ContenedorTabs extends Component{
         this.setState({
             usuario:valorTexto
         })
-        console.log(this.state) //Aquí se actualiza el estado.
      }
 
 
     agregarComentario () {
         let comentariosGuardados = this.state.usuarios;
         //console.log(comentariosGuardados);
-        console.log("This is the user: " +this.state.usuario);
+        //console.log("This is the user: " +this.state.usuario);
 
         comentariosGuardados.unshift({foto:'https://storage.googleapis.com/gweb-uniblog-publish-prod/static/blog/images/google-200x200.7714256da16f.png', nombre:'Zitle', comentario: this.state.usuario});
          console.log(comentariosGuardados);
@@ -67,24 +66,25 @@ export default class ContenedorTabs extends Component{
            usuarios: comentariosGuardados
         })
          console.log(this.state.usuarios);
-        //  imprimir(e);
+         
     }
-
-
-
+    
+    
+    imprimir(e){
+      let tecla = e.keyCode;
+      tecla != 13 ? this.obtenerValorTexto(e):this.agregarComentario();
+        
+      console.log(tecla);
+      this.actualizarContador(e);   
+    }
+    
+    
     openLeftMenu() {
       document.getElementById("leftMenu").style.display = "block";
     }
 
     closeLeftMenu() {
     document.getElementById("leftMenu").style.display = "none";
-    }
-
-    imprimir(e){
-      let tecla = e.keyCode;
-      tecla != 13 ? this.obtenerValorTexto(e):this.agregarComentario();
-
-      console.log(tecla);
     }
 
   render() {
@@ -130,7 +130,6 @@ export default class ContenedorTabs extends Component{
 				            <div className="pull-right">
 				            	<Caracteres pintar={this.state.contador}/>
 				           </div>
-				           <a href="#" className="w3-bar-item w3-button" onClick={this.agregarComentario}>Enlace2</a>
 				        </div>
 						<div className="row">
 							<div className="col-sm-12">
